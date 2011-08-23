57dbe9442e4f6bda415b0c1075cc47a6417fe19b
alex <alex@obout.ru>
2011-08-24 00:13:11 +0400

    Refactoring. Updated auto-complete.mode/Updated pymacs

---
+++ buildenv.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+CURRENT_PATH=$(pwd)
+VENV="$CURRENT_PATH/venv"
+PIP_PATH="$VENV/bin/pip"
+EMACS=emacs
+PYTHON="$VENV/bin/python"
+
+echo $VENV
+echo $CURRENT_PATH
+echo $PIP_PATH
+echo $PYTHON
+
+virtualenv --python=python2.6 --no-site-packages --clear $VENV
+source "$VENV/bin/activate"
+
+$VENV/bin/easy_install pip
+$PIP_PATH install -E $VENV -U ipython
+
+#$PIP_PATH install -E venv  http://pymacs.progiciels-bpi.ca/archives/Pymacs-0.23.tar.gz
+$PIP_PATH install -E $VENV -U -e hg+http://bitbucket.org/agr/rope#egg=rope
+rm -rf /tmp/rope
+mkdir /tmp/rope && cd /tmp/rope
+hg clone http://bitbucket.org/agr/ropemacs
+hg clone http://bitbucket.org/agr/ropemode
+git clone https://github.com/pinard/Pymacs.git
+ln -s ../ropemode/ropemode ropemacs/
+$PIP_PATH install -E $VENV -U /tmp/rope/ropemacs
+
+cd /tmp/rope/Pymacs/
+
+PYSETUP="$PYTHON $(pwd)/setup.py"
+PPPP="$PYTHON $(pwd)/pppp -C $(pwd)/ppppconfig.py"
+
+echo $PYSETUP
+echo $PPPP
+
+$PPPP *.in Pymacs contrib tests
+$PYSETUP install
+cp pymacs.el ~/.emacs.d/pymacs.el
+rm -f ~/.emacs.d/pymacs.elc
+
+#$PIP_PATH install -E $VENV -U /tmp/rope/Pymacs
+#./venv/bin/pip install -E venv -e svn+http://myrepo/svn/MyApp#egg=MyApp
--- emacs.el
+++ emacs.el
@@ -1,137 +1,107 @@
 (add-to-list 'load-path "~/.emacs.d/")
+(setq emacs-d-dir "~/.emacs.d/pylookup/")
 ;;(set-default-font "Consolas-8") ;;default font
 ;;the following is size 7 for me...
 (set-face-font 'default "-unknown-Envy Code R-normal-normal-normal-*-13-*-*-*-m-0-iso10646-1")
-;(set-frame-font "Envy Code R-7") ;; doesn't work consistently ;(
+;;(set-frame-font "Envy Code R-7") ;; doesn't work consistently ;(
 
-;;;(load-file "/usr/share/emacs23/site-lisp/cedet-common/cedet.el")
-;;;(add-to-list 'load-path "/usr/share/emacs23/site-lisp/ecb")
-;; Remote file editing via ssh
+;;(load-file "/usr/share/emacs23/site-lisp/cedet-common/cedet.el")
+;;(add-to-list 'load-path "/usr/share/emacs23/site-lisp/ecb")
+
+;;; Remote file editing via ssh
 (add-to-list 'load-path "~/.emacs.d/tramp-files/lisp")
 (add-to-list 'load-path "~/.emacs.d/slime/")
+;;(add-to-list 'load-path "~/.emacs.d/virtualenv/")
 ;;add jabber mode
-(add-to-list 'load-path "~/.emacs.d/emacs-jabber/")
+;;(add-to-list 'load-path "~/.emacs.d/emacs-jabber/")
 ;;(add-to-list 'load-path "~/.emacs.d/python-mode-6.0/")
+
+;;AUTO COMPLETE
+(add-to-list 'load-path "~/.emacs.d/auto-complete/")
+;;(add-to-list 'load-path "~/.emacs.d/auto-complete/ext/")
+
 (add-to-list 'load-path "~/.emacs.d/magit/")
 
 (setq pylookup-dir "~/.emacs.d/pylookup/")
-
 (add-to-list 'load-path pylookup-dir)
 
 (load-file "~/.emacs.d/nxml-mode/rng-auto.el")
 
-
+;;GLOBAL REQUIREMENTS
 (require 'font-lock) (if (fboundp 'global-font-lock-mode) (global-font-lock-mode 1))
 (require 'tramp)
 (require 'color-theme)
-(require 'auto-complete)
+;;(require 'auto-complete)
 (require 'auto-complete-config)
 (require 'yasnippet)
-;;(require 'python-mode)
+(require 'anything)
+(require 'ansi-color)
+(require 'smart-operator)
+
+;;MODES
+(require 'go-mode-load)
 (require 'css-mode)
 (require 'js2-mode)
-
-(require 'ansi-color)
 (require 'espresso)
 (require 'django-html-mode)
-(require 'smart-operator)
-;;(require 'ecb)
-(require 'pymacs)
-(require 'go-mode-load)
-;;(require 'zencoding-mode)
-(require 'rainbow-mode)
-(require 'http-twiddle)
 (require 'regex-tool)
 (require 'slime)
+(require 'magit)
+;;;(require 'jabber-autoloads)
+;;;(require 'python-mode)
+;;;(require 'ecb)
+;;;(require 'zencoding-mode)
+(require 'rainbow-mode)
+(require 'http-twiddle)
+
+;;;PYTHON REQUIREMENTS
 (require 'lambda-mode)
 (require 'python-pep8)
 (require 'python-pylint)
 (require 'hyperspec)
-(require 'jabber-autoloads)
-(require 'magit)
-
-(require 'anything)
-
 (require 'anything-ipython)
+(require 'pymacs)
 
 
 
-
-(add-hook 'lisp-mode-hook (lambda () (slime-mode t)))
-;;(add-hook 'inferior-lisp-mode-hook (lambda () (inferior-slime-mode t)))
-;; Optionally, specify the lisp program you are using. Default is "lisp"
-
-
+;;SET VARIABLES OF EMACS
 (setq tramp-default-method "ssh")
-
-(scroll-bar-mode nil) ;;hide scrroll bar
+(scroll-bar-mode nil) ;hide scrroll bar
 ;;(menu-bar-mode nil) ;this too
-(tool-bar-mode nil) ;;this too
-(column-number-mode t)    ;; but this one is good
+(tool-bar-mode nil) ;hide tool bar too
+;; Turn on column number mode
+(column-number-mode t) ;; but this one is good
 ;;(ido-mode t)
 
-;; Turn on column number mode
 (show-paren-mode t) ;;hightlight brackets
 (fset 'yes-or-no-p 'y-or-n-p)
 (setq inhibit-splash-screen t) ;;;do't show splash screen
 (setq default-tab-width 4)
+(modify-coding-system-alist 'file ".*" 'utf-8) ;; fuck cp1251 and koi-8
 
 ; Makes clipboard work
 (setq x-select-enable-clipboard t)
 (setq interprogram-paste-function 'x-cut-buffer-or-selection-value)
 
-(global-auto-complete-mode t)
-;;;(global-font-lock-mode 1)
+;;THEMES
 (setq color-theme-is-global t)
 (color-theme-initialize)
 (color-theme-solarized t)
 ;;;(color-theme-tango)
-;;;(global-auto-complete-mode t)
 
-(modify-coding-system-alist 'file ".*" 'utf-8) ;; fuck cp1251 and koi-8
+(setq browse-url-browser-function 'browse-url-generic
+      browse-url-generic-program "google-chrome")
 
-;;initialize yasnippet
+;;MODES VARIABLES
+(global-auto-complete-mode t)
+;;;(global-font-lock-mode 1)
+
+
+;; YASNIPPET
 (yas/initialize)
 (yas/load-directory "~/.emacs.d/snippets")
 
-
-(setq ipython-command "/var/github/python2.6/bin/ipython")
-;;(setq py-python-command-args '("-colors" "Linux"))
-;;(setq py-python-command "/var/github/python2.6/bin/ipython")
-;;(defadvice py-execute-buffer (around python-keep-focus activate)
-;; "return focus to python code buffer"
-;; (save-excursion ad-do-it))
-
-
-
-
-(defmacro lisp-slime (lisp path &optional coding)
- (let ((funname (intern (format "%s-slime" lisp))))
-   `(defun ,funname ()
-      (interactive)
-      (let ((inferior-lisp-program ,path)
-            (slime-net-coding-system (or ,coding 'utf-8-unix)))
-        (slime)))))
-
-(lisp-slime sbcl "/usr/bin/sbcl")
-(lisp-slime clisp "/usr/bin/clisp")
-
-
-(slime-setup '(slime-repl slime-js))
-
-(global-set-key [f5] 'slime-js-reload)
-(add-hook 'js2-mode-hook
-		  (lambda ()
-			(slime-js-minor-mode 1)))
-
-
-(defun hyperspec-lookup (&optional symbol-name)
- (interactive)
- (let ((browse-url-browser-function 'w3m-browse-url))
-   (if symbol-name
-       (common-lisp-hyperspec symbol-name)
-     (call-interactively 'common-lisp-hyperspec))))
-
+;; MODES AUTO DETECT
 ;;add-to-list 'auto-mode-alist '("\\.js$" . espresso-mode))
 (add-to-list 'auto-mode-alist '("\\.json$" . espresso-mode))
 ;;(autoload 'js2-mode "js2-mode" nil t)
@@ -141,93 +111,69 @@
 (add-to-list 'auto-mode-alist '("\\.css$" . css-mode))
 
 
-;;(setq py-python-command "/var/github/python2.6/bin/python2.6")
-;;(setq py-jython-command "/usr/bin/jython")
+;;LISP-MODE
+;;Common lisp compiler
+(defmacro lisp-slime (lisp path &optional coding)
+  (let ((funname (intern (format "%s-slime" lisp))))
+	`(defun ,funname ()
+	   (interactive)
+	   (let ((inferior-lisp-program ,path)
+			 (slime-net-coding-system (or ,coding 'utf-8-unix)))
+		 (slime)))))
 
-(when (require 'auto-complete nil t)
-  ;;(require 'auto-complete-yasnippet)
-  (require 'auto-complete-python)
-  ;;(require 'auto-complete-css)
-  ;;(require 'auto-complete-emacs-lisp)
-  ;;(require 'auto-complete-semantic)
-  ;;(require 'auto-complete-gtags)
-  (setq ac-auto-start 2)
-  (setq ac-dwim t)
-  (set-default 'ac-sources '(ac-source-words-in-buffer ac-source-symbols)))
+(lisp-slime sbcl "/usr/bin/sbcl")
+(lisp-slime clisp "/usr/bin/clisp")
 
-(add-hook 'css-mode-hook (lambda ()
-		   (message "css mode hook")
-		   (rainbow-mode t)
-		   ;;(setq ac-sources '(ac-source-words-in-buffer ac-source-symbols))))
-(setq ac-sources '(ac-source-css-keywords))))
-
-(add-hook 'lisp-mode-hook (lambda ()
-							(message "lisp mode hook")
-;;							 (setq ac-sources '(ac-source-words-in-buffer ac-source-symbols))))
-							(setq ac-sources '(ac-source-abbrev ac-source-words-in-buffer ac-source-files-in-current-dir ac-source-symbols ac-emacs-lisp-sources))
-							))
-
-(add-hook 'emacs-lisp-mode-hook (lambda ()
-							(message "emacs lisp mode hook")
-							;; (setq ac-sources '(ac-source-words-in-buffer ac-source-symbols))))
-;;							(setq ac-sources '(ac-source-abbrev ac-source-words-in-buffer ac-source-files-in-current-dir ac-source-symbols ac-emacs-lisp-aources))
-							))
+;;;SWANK-JS connection to slime
+;;(slime-setup '(slime-repl slime-js))
+;;(global-set-key [f5] 'slime-js-reload)
+;;(add-hook 'js2-mode-hook
+;;		  (lambda ()
+;;			(slime-js-minor-mode 1)))
 
 
-(add-hook 'python-mode-hook
-		  (lambda ()
-			(message "python mode hook")
-;;			(smart-operator-mode-on)
-;;			(setq 'ac-sources '(ac-source-ropemacs ))
-))
-(setq lambda-symbol (string (make-char 'greek-iso8859-7 107)))
-(add-hook 'python-mode-hook 'lambda-mode)
-(add-hook 'espresso-mode-hook (lambda ()
-                                (setq espresso-indent-level 2)))
 
-;;;(add-hook 'sgml-mode-hook 'zencoding-mode) ;; Auto-start on any markup modes
+;; AUTO COMPLETE CONFIGS
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/dict/")
+(setq ac-auto-start 3)
+(setq ac-auto-show-menu 0.8)
+(setq ac-quick-help-delay 0.5)
+(setq ac-dwim t)
+(ac-config-default)
 
-(add-hook 'python-mode-hook #'(lambda ()
-                                 (define-key py-mode-map (kbd "M-<tab>") 'anything-ipython-complete)))
-(add-hook 'ipython-shell-hook #'(lambda ()
-                                   (define-key py-mode-map (kbd "M-<tab>") 'anything-ipython-complete)))
+;; KEYMAPS
+(define-key ac-mode-map (kbd "C-c h") 'ac-last-quick-help)
+(define-key ac-mode-map (kbd "C-c H") 'ac-last-help)
+(define-key ac-mode-map (kbd "TAB") 'auto-complete)
+
+
+;; DEPRICATED FOR AUTO COMPLETE 1.4
+;; (when (require 'auto-complete nil t)
+;; ;;  (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+
+;;   (setq ac-auto-start 2)
+;;   (setq ac-dwim t)
+;;   ;;(require 'auto-complete-yasnippet)
+;;   (require 'auto-complete-python)
+;;   ;;(require 'auto-complete-css)
+;;   (require 'auto-complete-emacs-lisp)
+;;   ;;(require 'auto-complete-semantic)
+;;   ;;(require 'auto-complete-gtags)
+;;   (set-default 'ac-sources '(ac-source-words-in-buffer
+;; 							 ac-source-symbols)))
+
 
 (when (require 'anything-show-completion nil t)
-   (use-anything-show-completion 'anything-ipython-complete
-                                 '(length initial-pattern)))
-
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
-
-(eval-when-compile (require 'pylookup))
-
-;; set executable file and db file
-(setq pylookup-program (concat pylookup-dir "/pylookup.py"))
-(setq pylookup-db-file (concat pylookup-dir "/pylookup.db"))
-
-;; to speedup, just load it on demand
-(autoload 'pylookup-lookup "pylookup"
-  "Lookup SEARCH-TERM in the Python HTML indexes." t)
-
-(autoload 'pylookup-update "pylookup"
-  "Run pylookup-update and create the database at `pylookup-db-file'." t)
+  (use-anything-show-completion 'anything-ipython-complete
+								'(length initial-pattern)))
 
 
-
-(defun annotate-pdb ()
+(defun hyperspec-lookup (&optional symbol-name)
   (interactive)
-  (highlight-lines-matching-regexp "import pdb")
-  (highlight-lines-matching-regexp "pdb.set_trace()")
-  (highlight-lines-matching-regexp "import rpdb2")
-  (highlight-lines-matching-regexp "rpdb2.start_embedded_debugger("))
-
-(defun python-add-breakpoint ()
-  (interactive)
-  (py-newline-and-indent)
-  (insert "import ipdb; ipdb.set_trace()")
-  (highlight-lines-matching-regexp "^[ 	]*import ipdb; ipdb.set_trace()"))
-
-(define-key py-mode-map (kbd "C-c C-t") 'python-add-breakpoint)
-(add-hook 'python-mode-hook 'annotate-pdb)
+  (let ((browse-url-browser-function 'w3m-browse-url))
+	(if symbol-name
+		(common-lisp-hyperspec symbol-name)
+	  (call-interactively 'common-lisp-hyperspec))))
 
 
 ;;; Comment and uncomment function
@@ -253,8 +199,6 @@
     )
 )
 
-(setq browse-url-browser-function 'browse-url-generic
-      browse-url-generic-program "google-chrome")
 
 (defun autocompile ()
   "Compile itself if this is config file"
@@ -265,6 +209,54 @@
 	   (string-match ".emacs.d/themes/[a-zA-z_\+\-]*.el$" (buffer-file-name))
 	   )
       (byte-compile-file (buffer-file-name))))
+
+
+;;; HOOKS FOR MODES
+
+;;(add-hook 'inferior-lisp-mode-hook (lambda () (inferior-slime-mode t)))
+;; Optionally, specify the lisp program you are using. Default is "lisp"
+(add-hook 'lisp-mode-hook (lambda ()
+							(message "css mode hook")
+							(slime-mode t)))
+
+(add-hook 'css-mode-hook (lambda ()
+						   (message "css mode hook")
+						   (rainbow-mode t)
+						   ;;(setq ac-sources '(ac-source-words-in-buffer ac-source-symbols))))
+						   (setq ac-sources '(ac-source-css-keywords))))
+
+(add-hook 'lisp-mode-hook (lambda ()
+							(message "lisp mode hook")
+;;							 (setq ac-sources '(ac-source-words-in-buffer ac-source-symbols))))
+							(setq ac-sources '(ac-source-abbrev ac-source-words-in-buffer
+																ac-source-files-in-current-dir
+																ac-source-symbols ac-emacs-lisp-sources))
+							))
+
+(add-hook 'emacs-lisp-mode-hook (lambda ()
+								  (message "emacs lisp mode hook")
+								  ;; (setq ac-sources '(ac-source-words-in-buffer
+								  ;; 					 ac-source-symbols))
+								  ;; (setq ac-sources '(ac-source-abbrev
+								  ;; 					 ac-source-words-in-buffer
+								  ;; 					 ac-source-files-in-current-dir
+								  ;; 					 ac-source-symbols ac-emacs-lisp-aources))
+								  ))
+
+
+
+(add-hook 'espresso-mode-hook (lambda ()
+                                (setq espresso-indent-level 2)))
+
+;;;(add-hook 'sgml-mode-hook 'zencoding-mode) ;; Auto-start on any markup modes
+
+;;(add-hook 'python-mode-hook #'(lambda ()
+;;                                 (define-key py-mode-map (kbd "M-<tab>") 'anything-ipython-complete)))
+
+(add-hook 'ipython-shell-hook #'(lambda ()
+								  (define-key py-mode-map (kbd "M-<tab>") 'anything-ipython-complete)))
+
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 (add-hook 'after-save-hook 'autocompile)
 
