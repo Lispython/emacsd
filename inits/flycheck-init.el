a6f2bbe5f94653cd0630d18177f2a444759712fb
Alex <Alexandr.S.rus@gmail.com>
2016-08-30 10:29:33 +0300

    Update

--- inits/flycheck-init.el
+++ inits/flycheck-init.el
@@ -9,5 +9,12 @@
 (eval-after-load "flycheck"
   '(add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode))
 
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(flycheck-color-mode-line-error-face ((t (:inherit flycheck-fringe-error :foreground "dark red" :weight bold)))))
+
 (provide 'flycheck-init)
 ;;; flycheck-init.el ends here
