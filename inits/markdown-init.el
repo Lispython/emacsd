5927297c4c3f472759d48b79a1dffccaad82312e
Alex <Alexandr.S.rus@gmail.com>
2019-01-06 15:51:14 +0300

    Update markdown mode

--- inits/markdown-init.el
+++ inits/markdown-init.el
@@ -5,11 +5,11 @@
   :mode (("README\\.md\\'" . gfm-mode)
          ("\\.md\\'" . markdown-mode)
          ("\\.markdown\\'" . markdown-mode))
-  :init (setq markdown-command "docker run --rm -i emacsd pandoc --from markdown_github -t html5 --mathjax --highlight-style pygments --standalone"))
+  :init (setq markdown-command "docker run -i --rm emacsd pandoc --from markdown_github -t html5 --mathjax --highlight-style pygments --standalone"))
 
   ;;:init (setq markdown-command "docker run --rm -i emacsd markdown"))
 
 (message "markdown init loaded")
 
 (provide 'markdown-init)
-;;; crontab-init.el ends
+;;; markdown-init.el ends
