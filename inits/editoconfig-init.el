16b3e8d2af1b132682695f002de1d279c099c2f1
Alex <Alexandr.S.rus@gmail.com>
2019-01-13 22:42:41 +0300

    Update gitmodules.

--- inits/editoconfig-init.el
+++ inits/editoconfig-init.el
@@ -1,6 +1,21 @@
-(load-file "~/.emacs.d/ext/editorconfig/editorconfig.el")
+;;; editoconfig-init.el ---
+
+;;; Commentary:
+;;
+
+;;; Code:
+
+;;(load-file "~/.emacs.d/ext/editorconfig/editorconfig.el")
 
 ;; (require 'editorconfig)
-(editorconfig-mode t)
+;;(editorconfig-mode t)
 
-(message "editorconfig init loaded")
+;;(message "editorconfig init loaded")
+
+;; (use-package editorconfig
+;;   :config (editorconfig-mode t)
+;;   )
+
+(provide 'editoconfig-init)
+
+;;; editoconfig-init.el ends here
