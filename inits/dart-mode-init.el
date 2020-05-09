2d4d53af80bcf83c08c70b43a2f72303cfe7c6a4
Alex <Alexandr.S.rus@gmail.com>
2020-05-09 15:25:51 +0300

    Updated dart

--- inits/dart-mode-init.el
+++ inits/dart-mode-init.el
@@ -9,15 +9,30 @@
 
 (use-package dart-mode
   :ensure t
+  ;; :hook (dart-mode . flutter-test-mode)
   :config (progn
             (add-to-list 'auto-mode-alist '("\\.dart\\'" . dart-mode))
             ;; (add-to-list 'eglot-server-programs '(dart-mode . ("dart_language_server")))
 
+            (add-to-list 'eglot-server-programs '(dart-mode . ("dart_language_server")))
+
+            (add-hook 'dart-mode-hook 'eglot-ensure)
             )
   )
 
 
+(use-package flutter
+  :after dart-mode
+  :bind (:map dart-mode-map
+              ("C-M-x" . #'flutter-run-or-hot-reload))
+  :custom
+  (flutter-sdk-path "/Applications/flutter/"))
 
+
+(use-package flutter-l10n-flycheck
+  :after flutter
+  :config
+  (flutter-l10n-flycheck-setup))
 
 
 (provide 'dart-mode-init)
